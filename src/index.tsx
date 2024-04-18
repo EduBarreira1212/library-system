c2aaab4161519f97ba2c29aa4be502695d99d05a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-18 10:44:34 -0300

    feat: routes created and configured

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ShowList from './routes/ShowList';
+import Register from './routes/Register';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <ShowList/>
+      },
+      {
+        path: "/register",
+        element: <Register/>
+      }
+    ]
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
