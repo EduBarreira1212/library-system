3822ef3756ec87796f2cd6d9bce8410d87418000
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-20 14:42:00 -0300

    feat: formik component configured

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,7 +1,21 @@
+import { Form, Formik } from "formik";
+
 const Register = () => {
     return (
         <div>
             <h1>Register</h1>
+            <Formik
+                initialValues={{
+                    name: "", cpf: "", email: "", phone: "",
+                    title: "", author: "", publishingCompany: "", publicationYear: "",
+                    loan: ""
+                }}
+                onSubmit={(values) => console.log(values)}
+            >
+                <Form>
+                    
+                </Form>
+            </Formik>
         </div>
     );
 }
