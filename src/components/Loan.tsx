f64919595b34133c215ed723a0e7e2055f855bc0
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-21 17:00:50 -0300

    feat: component's content added

--- src/components/Loan.tsx
+++ src/components/Loan.tsx
@@ -6,7 +6,18 @@ interface IloanProps{
 const Loan: React.FC<IloanProps> = ({children}) => {
     return(
         <div>
-            
+            <h3>Person:</h3>
+            <p>{children.name}</p>
+            <p>{children.cpf}</p>
+            <p>{children.email}</p>
+            <p>{children.phone}</p>
+            <h3>Book:</h3>
+            <p>{children.title}</p>
+            <p>{children.author}</p>
+            <p>{children.publisher}</p>
+            <p>{children.publicationYear}</p>
+            <h3>Loan:</h3>
+            <p>{children.loan.toLocaleString()}</p>
         </div>
     );
 }
