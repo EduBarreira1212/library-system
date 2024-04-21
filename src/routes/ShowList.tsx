98d0a291175b9cc9c8e8687bfa34958458213690
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-21 16:27:06 -0300

    feat: function to get data created

--- src/routes/ShowList.tsx
+++ src/routes/ShowList.tsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ShowList = () => {
     const [list, setList] = useState();
     const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("https://apigenerator.dronahq.com/api/PxY0zUv0/libraryData");
+                setList(response.data);
+                setIsLoading(false);
+            } catch (error) {
+                console.log("Error fetching data:", error);
+            }
+        };
+        fetchData();
+    },[]);
 
     return (
         <div>
