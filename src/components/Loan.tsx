b48f285c253d7e8da06d667a57c7dad5a6228a32
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-21 17:30:02 -0300

    feat: styled p added

--- src/components/Loan.tsx
+++ src/components/Loan.tsx
@@ -5,9 +5,21 @@ const Div = styled.div`
     background-color: #f9f9f9;
     border: 0.1vw solid #ddd;
     border-radius: 0.8vw;
-    padding: 3vh;
+    padding: 2vh;
     margin-bottom: 3vh;
     box-shadow: 0 0.4vh 0.6vw rgba(0, 0, 0, 0.1);
+`;
+
+const H3 = styled.h3`
+    font-size: 1.8vw;
+    color: #666;
+    margin-bottom: 0.5vw;
+`;
+
+const P = styled.p`
+    font-size: 1.5vw;
+    color: #333;
+    margin-bottom: 0.25vw;
 `;
 
 interface IloanProps{
@@ -16,18 +28,18 @@ interface IloanProps{
 const Loan: React.FC<IloanProps> = ({children}) => {
     return(
         <Div>
-            <h3>Person:</h3>
-            <p>{children.name}</p>
-            <p>{children.cpf}</p>
-            <p>{children.email}</p>
-            <p>{children.phone}</p>
-            <h3>Book:</h3>
-            <p>{children.title}</p>
-            <p>{children.author}</p>
-            <p>{children.publisher}</p>
-            <p>{children.publicationYear}</p>
-            <h3>Loan:</h3>
-            <p>{children.loan.toLocaleString()}</p>
+            <H3>Person:</H3>
+            <P>{children.name}</P>
+            <P>{children.cpf}</P>
+            <P>{children.email}</P>
+            <P>{children.phone}</P>
+            <H3>Book:</H3>
+            <P>{children.title}</P>
+            <P>{children.author}</P>
+            <P>{children.publisher}</P>
+            <P>{children.publicationYear}</P>
+            <H3>Loan:</H3>
+            <P>{children.loan.toLocaleString()}</P>
         </Div>
     );
 }
