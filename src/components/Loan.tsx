3ab25e7ddb070c02d4a898f6641e1cbe49a39826
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-21 17:23:08 -0300

    feat: styled div added

--- src/components/Loan.tsx
+++ src/components/Loan.tsx
@@ -1,11 +1,21 @@
+import styled from "styled-components";
 import { IformData } from "../routes/Register";
+
+const Div = styled.div`
+    background-color: #f9f9f9;
+    border: 0.1vw solid #ddd;
+    border-radius: 0.8vw;
+    padding: 3vh;
+    margin-bottom: 3vh;
+    box-shadow: 0 0.4vh 0.6vw rgba(0, 0, 0, 0.1);
+`;
 
 interface IloanProps{
     children: IformData
 }
 const Loan: React.FC<IloanProps> = ({children}) => {
     return(
-        <div>
+        <Div>
             <h3>Person:</h3>
             <p>{children.name}</p>
             <p>{children.cpf}</p>
@@ -18,7 +28,7 @@ const Loan: React.FC<IloanProps> = ({children}) => {
             <p>{children.publicationYear}</p>
             <h3>Loan:</h3>
             <p>{children.loan.toLocaleString()}</p>
-        </div>
+        </Div>
     );
 }
 
