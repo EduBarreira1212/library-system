49fccea2c729bce402519fb9d397b7d99b1e460f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-21 16:48:10 -0300

    feat: loan component created

---
+++ src/components/Loan.tsx
@@ -0,0 +1,12 @@
+import { IformData } from "../routes/Register";
+
+interface IloanProps{
+    children: IformData
+}
+const Loan: React.FC<IloanProps> = ({children}) => {
+    return(
+        <div>
+            
+        </div>
+    );
+}
