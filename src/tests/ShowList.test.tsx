1c46f49edda3279a4d2fcba107f074d132b34822
Eduardo Barreira <edubarreira1212@gmail.com>
2024-05-16 10:41:24 -0300

    test: render component test

--- src/tests/ShowList.test.tsx
+++ src/tests/ShowList.test.tsx
@@ -9,4 +9,19 @@ describe("show list component", () => {
         const loading = screen.getByText("Loading...");
         expect(loading).toBeInTheDocument();
     })
+    test("render component", async () => {
+        render(<BrowserRouter><ShowList/></BrowserRouter>);
+        const label = await screen.findByText("Filter:");
+        const label2 = await screen.findByText("Filter by:");
+        const cpf = await screen.findByText("CPF");
+        const date = await screen.findByText("Date");
+        const publication = await screen.findByText("Publication year");
+        const btn = await screen.findByText("Register a loan");
+        expect(label).toBeInTheDocument();
+        expect(label2).toBeInTheDocument();
+        expect(cpf).toBeInTheDocument();
+        expect(date).toBeInTheDocument();
+        expect(publication).toBeInTheDocument();
+        expect(btn).toBeInTheDocument();
+    })
 })
